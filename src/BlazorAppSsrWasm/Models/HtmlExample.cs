07d1a5cada3dae5e7c3184d74c2f4fecdbbe1eef
Johan Kronberg <johan.kronberg@krompaco.nu>
2024-03-01 16:47:18 +0100

    Add support to open a library component example on its own page

--- src/BlazorAppSsrWasm/Models/HtmlExample.cs
+++ src/BlazorAppSsrWasm/Models/HtmlExample.cs
@@ -2,6 +2,11 @@ namespace BlazorAppSsrWasm.Models;
 
 public class HtmlExample
 {
+    public HtmlExample(Type type)
+    {
+        this.TypeFullName = type?.FullName ?? string.Empty;
+    }
+
     public string Variation { get; set; } = "Default";
 
     public string Name { get; set; } = string.Empty;
@@ -11,4 +16,6 @@ public class HtmlExample
     public string Description { get; set; } = string.Empty;
 
     public string Html { get; set; } = string.Empty;
+
+    public string TypeFullName { get; private set; }
 }
